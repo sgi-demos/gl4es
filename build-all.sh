22cc5786b83e705466ebc9ea043199a4e8b09786
sgi-demos <sgi.demos@gmail.com>
2026-07-02 09:49:05 -0400

    Add clean-all.sh and make buid-all.sh verbose

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# build-with-external-gles2.sh — configure & build gl4es with an external gles2
+set -o verbose
 rm -rf build && mkdir build && cd build
 cmake -DNOX11=ON -DNOEGL=ON -DDEFAULT_ES=2 \
       -DSTATICLIB=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
---
+++ clean-all.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -o verbose
+rm -rf build
+rm -f lib/libGL-native.a
+
+rm -rf build-web
+rm -f lib/libGL-web.a
