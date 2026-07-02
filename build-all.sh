cd3739ee62cde7361a96372058bb2b2924761b10
sgi-demos <sgi.demos@gmail.com>
2026-07-02 10:57:14 -0400

    Polish build-all.sh - no functional change

--- build-all.sh
+++ build-all.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 set -o verbose
+
+#
+# native
+#
 rm -rf build && mkdir build && cd build
-cmake -DNOX11=ON -DNOEGL=ON -DDEFAULT_ES=2 \
-      -DSTATICLIB=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+cmake -DNOX11=ON \
+      -DNOEGL=ON \
+      -DDEFAULT_ES=2 \
+      -DSTATICLIB=ON \
+      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+      ..
 make
 cd ..
 mv lib/libGL.a lib/libGL-native.a
 
+#
+# web
+#
 rm -rf build-web && mkdir build-web && cd build-web
-emcmake cmake -DNOX11=ON -DNOEGL=ON -DDEFAULT_ES=2 -DSTATICLIB=ON ..
+emcmake cmake -DNOX11=ON \
+              -DNOEGL=ON \
+              -DDEFAULT_ES=2 \
+              -DSTATICLIB=ON \
+              ..
 emmake make
 cd ..
 mv lib/libGL.a lib/libGL-web.a
